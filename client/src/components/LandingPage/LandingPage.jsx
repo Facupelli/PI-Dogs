862cfe4a49832569b96da8e1580d7f6dea85cbda
Facundo Pellicer <facundopellicer4@gmail.com>
2021-10-06 20:29:48 -0300

    fix landing page background-image

--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import s from './LandingPage.module.css';
+import s from "./LandingPage.module.css";
 
 export default function LandingPage() {
   return (
-    <div className={s.div}>
-      <p>Welcome to the</p>
-      <h1>DOG APP</h1>
-      <Link to="/home">
-        <button>INGRESAR</button>
-      </Link>
+    <div className={s.background}>
+      <div className={s.welcome}>
+        <p>Welcome to the</p>
+        <h1>DOG APP</h1>
+        <Link to="/home">
+          <button>INGRESAR</button>
+        </Link>
+      </div>
     </div>
   );
 }
