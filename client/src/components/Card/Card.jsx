58dfe509878e1cef0e517d7449e4da1e007940b7
Facundo Pellicer <facundopellicer4@gmail.com>
2021-10-07 11:30:20 -0300

    style card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import s from './Card.module.css'
+import s from "./Card.module.css";
 
 export default function Card({
   id,
@@ -10,25 +10,32 @@ export default function Card({
   min_weight,
   max_weight,
 }) {
-
   let picture;
-  if(!image){
-    picture = 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f540ef58358961.59f95102c77f3.jpg';
-  }else{
+  if (!image) {
+    picture =
+      "https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f540ef58358961.59f95102c77f3.jpg";
+  } else {
     picture = image;
   }
 
   return (
     <div className={s.card}>
-      <Link to={"/home/" + id}>
-        <img src={picture} alt="not found" width="200px" height="200px" />
-        <h3>{name}</h3>
-      </Link>
-      <h4>{temperament}</h4>
-      <h4>Weight</h4>
       <div>
-        <p>Min: {min_weight} kg </p>
-        <p>Max: {max_weight} kg </p>
+        <Link to={"/home/" + id}>
+          <img src={picture} alt="not found" width="200px" height="200px" />
+        </Link>
+      </div>
+      <div>
+        <Link to={"/home/" + id}>
+          <h3>{name}</h3>
+        </Link>
+        <h4>{temperament}</h4>
+        <div className={s.container}>
+          <p className={s.weight}>Weight:</p>
+          <p className={s.number}>
+            {min_weight} - {max_weight} kg
+          </p>
+        </div>
       </div>
     </div>
   );
