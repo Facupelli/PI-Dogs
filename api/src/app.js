bd01f6743a7472fda20c902d56decf585a52ccf0
Facupelli <facundopellicer4@gmail.com>
2021-11-22 19:25:30 -0300

    deploy config changes fede tuto

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,6 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
-
 const server = express();
 const cors = require("cors");
 const { ORIGIN } = process.env;
@@ -16,15 +15,17 @@ const corsOptions = {
 
 server.name = "API";
 
-server.set("port", process.env.PORT || 3001); // permite que la nube asigne un port cuando deploye
+server.listen(process.env.PORT || 3001, () => {
+  console.log(`%s listening at ${process.env.PORT ? process.env.PORT : 3001}`);
+}); // permite que la nube asigne un port cuando deploye
 server.use(cors(corsOptions));
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  // res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
